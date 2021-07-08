266651070fae54def974818686e78376ea39749b
Zero Dragon <carlos@floresbenavides.com>
2021-07-07 20:19:19 -0500

    Autogets tags when there are not defined

--- src/reader.js
+++ src/reader.js
@@ -2,14 +2,28 @@ const icy = require('icy')
 const mutag = require('mutag')
 const fs = require('fs')
 const _path = require('path')
+const metadater = require('./metadater')
+
+const songsPath = _path.join(__dirname, '../songs/')
+
+const tagGetter = filePath => {
+  const file = fs.readFileSync(filePath)
+  return mutag.fetch(file)
+    .catch(err => {
+      console.log('[INFO] file does not have MP3 tags getting them')
+      return metadater(filePath).then(result => {
+        return tagGetter(filePath)
+      })
+    })
+}
 
 const reader = async url => {
   const metadata = await new Promise(resolve => {
     icy.get(url, response => {
       response.on('metadata', async metadata => {
         const { StreamTitle: id } = icy.parse(metadata)
-        const file = fs.readFileSync(_path.resolve(__dirname, '../', 'songs/', `${id}.mp3`))
-        const tags = await mutag.fetch(file)
+        const filePath = _path.resolve(songsPath, `${id}.mp3`)
+        const tags = await tagGetter(filePath)
         resolve({ id, artist: tags.TPE1, title: tags.TIT2 })
       })
     })
