6b6c78f1d2c1dce6b4b84e3c1b0edd4a2d15a6d1
ChewRafa <yphar89@gmail.com>
2021-07-08 14:04:47 -0500

    Adds '/help' command

--- src/bot.js
+++ src/bot.js
@@ -3,12 +3,12 @@ const downloader = require('./downloader')
 const { queue } = require('./playlist-manager')
 
 const commands = {
-  '/skipsong' (ctx, response = false) {
+  '/skipsong'(ctx, response = false) {
     elBot.liveStream.nextSong()
     if (response) ctx.reply('Skipping')
   },
 
-  async '/addsong' (ctx, query) {
+  async '/addsong'(ctx, query) {
     const song = query.join(' ')
     if (!song) return ctx.reply('¯\_(ツ)_/¯ seriously?')
     const { message_id: msgId } = await ctx.reply('Looking...')
@@ -22,18 +22,18 @@ const commands = {
         ctx.reply(response)
       })
   },
-  
-  '/flush' (ctx, response = false) {
+
+  '/flush'(ctx, response = false) {
     if (response) ctx.reply('Flusing Playlist')
     elBot.liveStream.flushPlayList()
   },
 
-  '/startstream' (ctx) {
+  '/startstream'(ctx) {
     ctx.reply('Stream started')
     elBot.liveStream.startStream()
   },
 
-  '/stop' (ctx) {
+  '/stop'(ctx) {
     const clearPl = '#EXTM3U\n'
     fs.writeFileSync(elBot.liveStream.configuration.filename, clearPl)
     commands['/flush'](ctx, false)
@@ -41,24 +41,37 @@ const commands = {
     ctx.reply('Stopping stream')
   },
 
-  async '/queue' (ctx) {
+  async '/queue'(ctx) {
     const { message_id: msgId } = await ctx.reply('Hold on...')
     await ctx.reply(await queue())
     ctx.deleteMessage(msgId)
   },
 
-  async '/nowplaying' (ctx) {
+  async '/nowplaying'(ctx) {
     const { message_id: msgId } = await ctx.reply('Hold on...')
     ctx.reply(await queue(true))
     ctx.deleteMessage(msgId)
+  },
+
+  '/help'(ctx) {
+    ctx.reply(`Available commands:
+      **/help**   ---- shows this message.
+      **/addsong** __nameOfTheSongORyoutubeURL__  ---- adds the song to the queue. If not found, returns a failure message.
+      **/skipsong** ---- plays next song in queue.
+      **/nowplaying**  ---- returns the current song playing.
+      **/queue** ---- shows the next 10 songs information, plus the remaining songs on queue.
+      **/startstream** ----starts playing the songs in queue.
+      **/flush** ---- clears the playlist.
+      **/stop** ---- stops the music.
+  `)
   }
 }
 
 const elBot = {
-  setStream (liveStream) {
+  setStream(liveStream) {
     this.liveStream = liveStream
   },
-  dispatchCommand (ctx, command, params) {
+  dispatchCommand(ctx, command, params) {
     const cmd = command.toLowerCase()
     if (commands[cmd])
       return commands[cmd](ctx, params)
